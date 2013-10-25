c33dc0f3287054f98ebc6cbd3174d6f6dfa8c1ba
seba-1511 <seba-1511@hotmail.com>
2013-10-26 01:49:11 +0200

    weather until end 2008

--- stockMarket/getData/getWeather.py
+++ stockMarket/getData/getWeather.py
@@ -26,8 +26,8 @@ wae = wap.WolframAlphaEngine(appid, server)
 month = 12
 day = 24
 year = 2010
-for year in range(2006, 2007):
-	for month in range(1, 13):
+for year in range(2008, 2009):
+	for month in range(7, 13):
 		for day in range(1, 31):
 			if not(month == 2 and day > 28):
 				print 'Results for:' + input + str(day) + '/' + str(month) + '/' + str(year)
