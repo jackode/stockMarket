567dfa55a4cf29ed3e2c53d0f24dee36e544bdd1
seba-1511 <seba-1511@hotmail.com>
2013-11-04 17:26:22 +0100

    Started implementation as DB.

---
+++ stockMarket/getData/buildDatabaseFromCSV.py
@@ -0,0 +1,2 @@
+#-*- coding: utf-8 -*-
+from getData.models import *
--- stockMarket/getData/models.py
+++ stockMarket/getData/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Weather(models.Model):
     temperature = models.SmallIntegerField(null=True, blank=True)
     humidity = models.SmallIntegerField(null=True, blank=True)
@@ -15,11 +16,44 @@ class Weather(models.Model):
     def __unicode__(self):
         return u'' + str(self.day) + '/' + str(self.month) + '/' + str(self.year)
 
+
 class Stock(models.Model):
-    day = models.SmallIntegerField(null=True, blank=True)
-    month = models.SmallIntegerField(null=True, blank=True)
-    year = models.SmallIntegerField(null=True, blank=True)
+    day = models.SmallIntegerField()
+    month = models.SmallIntegerField()
+    year = models.SmallIntegerField()
     value = models.SmallIntegerField(null=True, blank=True)
+    volume = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
 
     def __unicode__(self):
         return u'' + str(self.day) + '/' + str(self.month) + '/' + str(self.year)
+
+
+class TyroonStock(Stock):
+    pass
+
+
+class WarrentStock(Stock):
+    pass
+
+
+class IndianStock(Stock):
+    pass
+
+
+class TenRenStock(Stock):
+    pass
+
+
+class DianaStock(Stock):
+    pass
+
+
+class dWeather(Weather):
+    pass
+
+
+class ddWeather(Weather):
+    pass
