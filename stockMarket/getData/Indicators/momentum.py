6943d185d5000edf46adbcf0b171958a2162c7cb
seba-1511 <seba-1511@hotmail.com>
2013-11-05 18:02:25 +0100

    Added accumulation distribution line

--- stockMarket/getData/Indicators/momentum.py
+++ stockMarket/getData/Indicators/momentum.py
@@ -5,35 +5,15 @@ import numpy as np
 
 # db = TyroonStock
 # tyroon = db.objects.all()
-tyroon = sp.genfromtxt('../CSV Data/dbDump/stock_tyroon.csv', delimiter=';')
+tyroon = sp.genfromtxt('../CSV Data/dbDump/stock_diana.csv', delimiter=';')
 N = 14
 day, month, year, open, close, low, high, adj, volume = 1, 2, 3, 4, 5, 6, 7, 8, 9
 i = 0
 previousEMA = 0.0
-extremePoint = previousSar = previousAF = 0.0
-
-
-def getNDayAgo(n, day=0, month=0, year=0):
-    return dayClose[-n]
-    # if day <= n:
-    #     day = 30 + day - n
-    #     if month == 1:
-    #         month = 12
-    #         year = year - 1
-    #     else:
-    #         month = month - 1
-    # else:
-    #     day = day - n
-    # try:
-    #     return db.objects.get(day=day, month=month, year=year)
-    # except:
-    #     try:
-    #         day = day + 2
-    #         return db.objects.get(day=day, month=month, year=year)
-    #     except:
-    #         return None
+extremePoint = previousSar = previousAF = previousADL = 0.0
 
 # Testing only:
+# Also, CHANGE DIANA WITH TYROON !
 tyroon = tyroon[:25]
 #
 
@@ -45,50 +25,54 @@ for t in tyroon:
         year=t[year]
     )
 
-    # Momentum:
-    if i >= N:
-        entry.momentum = (t[close] / tyroon[i - N][close]) * 100
-
-    # 5 day disparity:
-    if i >= 5:
-        temp = np.mean(tyroon[i - 5:i, close])
-        entry.day5disparity = (t[close] / temp) * 100
-
-    # 10 day disparity:
-    if i >= 10:
-        temp = np.mean(tyroon[i - 10:i, close])
-        entry.day10disparity = (t[close] / temp) * 100
-
-    #Stochastik % K
-    if i >= N:
-        B = np.amin(tyroon[i - N:i, close])
-        H = np.amax(tyroon[i - N:i, close])
-        entry.stochK = 100 * (t[close] - B) / (H - B)
-
-    # Price Volume Trend:
-    if i >= 1:
-        entry.priceVolumeTrend = t[
-            volume] * (t[close] - tyroon[i - 1][close]) / tyroon[i - 1][close]
-
     # Exponential Moving Average:
     smoothing = 2.0 / 11.0
     previousEMA = ((t[close] - previousEMA) * smoothing) + previousEMA
     entry.movAverageExp = previousEMA
 
-    # Parabolic SAR
-    if i > N: 
-        if extremePoint < np.amax(tyroon[i-N, high]):
-            extremePoint = np.amax(tyroon[i-N, high])
-            previousAF = previousAF + 0.02 if (previousAF + 0.02) <= 0.2 else 0.2
+    if i >= 1:
+        # Price Volume Trend:
+        entry.priceVolumeTrend = t[
+            volume] * (t[close] - tyroon[i - 1][close]) / tyroon[i - 1][close]
+
+    if i >= 5:
+        # 5 day disparity:
+        temp = np.mean(tyroon[i - 5:i, close])
+        entry.day5disparity = (t[close] / temp) * 100
+
+    if i >= 10:
+        # 10 day disparity:
+        temp = np.mean(tyroon[i - 10:i, close])
+        entry.day10disparity = (t[close] / temp) * 100
+
+    if i >= N:
+        Low = np.amin(tyroon[i - N:i, close])
+        High = np.amax(tyroon[i - N:i, close])
+
+        #Momentum:
+        entry.momentum = (t[close] / tyroon[i - N][close]) * 100
+
+        #Stochastik % K
+        entry.stochK = 100 * (t[close] - Low) / (High - Low)
+
+        #Parabolic SAR
+        if extremePoint < np.amax(tyroon[i - N, high]):
+            extremePoint = np.amax(tyroon[i - N, high])
+            previousAF = previousAF + \
+                0.02 if (previousAF + 0.02) <= 0.2 else 0.2
         if i == 0:
             previousSar = t[low]
         else:
-            previousSar = previousSar + previousAF*(extremePoint - previousSar)
+            previousSar = previousSar + \
+                previousAF * (extremePoint - previousSar)
         entry.paraSar = previousSar
 
-    #Accumulation Distribution Line:
-    
-    # entry.accDistrLine = 
+        # Accumulation Distribution Line:
+        mFlowMultplier = ((t[close] - Low) - (High - t[close])) / (High - Low)
+        mFlowVolume = mFlowMultplier * sp.sum(tyroon[i -N:i, volume])
+        previousADL = previousADL + mFlowVolume
+        entry.accDistrLine = previousADL
+
 
     entry.save()
     i = i + 1
