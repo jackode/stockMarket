9bf8ebc250050daea526751568631ae2c13e3fda
Sébastien Arnold <seba-1511@2754171.no-reply.drupal.org>
2013-11-20 23:28:11 +0100

    Fixed bugs, 3 classes, shuffled and error on whole test set.

--- stockMarket/learning/learning.py
+++ stockMarket/learning/learning.py
@@ -109,7 +109,8 @@ cvFeatures = np.asarray(cvFeatures)
 cvAnswers = np.asarray(cvAnswers)
 
 lrLearner = LogisticRegression(penalty='l2', dual=True, C=1.0)
-svmLearner = svm.SVC(C=1.0, kernel="poly", degree=5, gamma=0.0, probability=True)
+svmLearner = svm.SVC(C=1.0, kernel="linear", gamma=0.0, probability=True)
+#Best when only binary classification svmLearner = svm.SVC(C=1.0, kernel="poly", degree=5, gamma=0.0, probability=True)
 knnLearner = neighbors.KNeighborsClassifier(n_neighbors=350)
 
 start = 10
@@ -153,13 +154,13 @@ print 'Plotting the obtained results...'
 curr_dir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 execfile(curr_dir + '/../Scripts/plot.py')
 
-lrPrecDown, lrRecallDown, lrThresholdsDown = precision_recall_curve(testAnswers, lrLearner.predict_proba(testFeatures)[:, 0])
-svmPrecDown, svmRecallDown, svmThresholdsDown = precision_recall_curve(testAnswers, svmLearner.predict_proba(testFeatures)[:, 0])
-knnPrecDown, knnRecallDown, knnThresholdsDown = precision_recall_curve(testAnswers, knnLearner.predict_proba(testFeatures)[:, 0])
+lrPrecDown, lrRecallDown, lrThresholdsDown = precision_recall_curve(testAnswers == -1, lrLearner.predict_proba(testFeatures)[:, 1])
+svmPrecDown, svmRecallDown, svmThresholdsDown = precision_recall_curve(testAnswers == -1, svmLearner.predict_proba(testFeatures)[:, 1])
+knnPrecDown, knnRecallDown, knnThresholdsDown = precision_recall_curve(testAnswers == -1, knnLearner.predict_proba(testFeatures)[:, 1])
 
-lrPrecUp, lrRecallUp, lrThresholdsUp = precision_recall_curve(testAnswers, lrLearner.predict_proba(testFeatures)[:, 1])
-svmPrecUp, svmRecallUp, svmThresholdsUp = precision_recall_curve(testAnswers, svmLearner.predict_proba(testFeatures)[:, 1])
-knnPrecUp, knnRecallUp, knnThresholdsUp = precision_recall_curve(testAnswers, knnLearner.predict_proba(testFeatures)[:, 1])
+lrPrecUp, lrRecallUp, lrThresholdsUp = precision_recall_curve(testAnswers == 1, lrLearner.predict_proba(testFeatures)[:, 2])
+svmPrecUp, svmRecallUp, svmThresholdsUp = precision_recall_curve(testAnswers == 1, svmLearner.predict_proba(testFeatures)[:, 2])
+knnPrecUp, knnRecallUp, knnThresholdsUp = precision_recall_curve(testAnswers == 1, knnLearner.predict_proba(testFeatures)[:, 2])
 
 plotLines([[lrRecallDown], [lrPrecDown]], 'LR: Precision vs Recall(Down)', 'Recall', 'Precision')
 plotLines([[svmRecallDown], [svmPrecDown]], 'SVM: Precision vs Recall(Down)', 'Recall', 'Precision')
@@ -175,16 +176,16 @@ plotLines([[knnIndices, knnIndices], [knnTrainingError, knnTestingError]], 'Erro
 
 #Printing Classification Report:
 print 'Report for Logistic Regression (Up):'
-print classification_report(testAnswers, lrLearner.predict_proba(testFeatures)[:, 1] > 0.69, target_names=['Don\'t know', 'Will go up'])
+print classification_report(testAnswers == 1, lrLearner.predict_proba(testFeatures)[:, 2] > 0.69, target_names=['Don\'t know', 'Will go up'])
 print 'Report for Logistic Regression (Down):'
-print classification_report(testAnswers, lrLearner.predict_proba(testFeatures)[:, 0] > 0.6, target_names=['Don\'t know', 'Will go Down'])
+print classification_report(testAnswers == -1, lrLearner.predict_proba(testFeatures)[:, 1] > 0.6, target_names=['Don\'t know', 'Will go Down'])
 
 print 'Report for SVM (Up):'
-print classification_report(testAnswers, svmLearner.predict_proba(testFeatures)[:, 1] > 0.53, target_names=['Don\'t know', 'Will go up'])
+print classification_report(testAnswers == 1, svmLearner.predict_proba(testFeatures)[:, 2] > 0.53, target_names=['Don\'t know', 'Will go up'])
 print 'Report for SVM (Down):'
-print classification_report(testAnswers, svmLearner.predict_proba(testFeatures)[:, 0] > 0.46, target_names=['Don\'t know', 'Will go Down'])
+print classification_report(testAnswers == -1, svmLearner.predict_proba(testFeatures)[:, 1] > 0.46, target_names=['Don\'t know', 'Will go Down'])
 
 print 'Report for kNN (Up):'
-print classification_report(testAnswers, knnLearner.predict_proba(testFeatures)[:, 1] > 0.56, target_names=['Don\'t know', 'Will go up'])
+print classification_report(testAnswers == 1, knnLearner.predict_proba(testFeatures)[:, 2] > 0.56, target_names=['Don\'t know', 'Will go up'])
 print 'Report for kNN (Down):'
-print classification_report(testAnswers, knnLearner.predict_proba(testFeatures)[:, 0] > 0.3, target_names=['Don\'t know', 'Will go Down'])
+print classification_report(testAnswers == -1, knnLearner.predict_proba(testFeatures)[:, 1] > 0.3, target_names=['Don\'t know', 'Will go Down'])
